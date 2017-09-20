9566e009b0fa23c6dd9942a48c2a7d2892bdb3a5
Max <MaxMB15@gmail.com>
2017-09-19 23:04:43 -0400

    added comments on what to do in the future

--- src/Expression.java
+++ src/Expression.java
@@ -3,6 +3,8 @@
 //This class is in charge of handling multiple NumberVar and Operands
 public class Expression {
 	public Expression(String s){
+		//I have to implement a parentheses stack
+		
 		
 	}
 }

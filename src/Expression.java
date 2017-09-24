e4c8fb84b5979ada54e8cc1bff75774c4dd5697c
Max <MaxMB15@gmail.com>
2017-09-23 23:57:28 -0400

    changed some things with expression class

--- src/Expression.java
+++ src/Expression.java
@@ -1,9 +1,13 @@
 
-
 //This class is in charge of handling multiple NumberVar and Operands
 public class Expression {
 	public Expression(String s){
 		//I have to implement a parentheses stack
+		//One teset
+		
+		
+		
+		
 		
 		
 	}

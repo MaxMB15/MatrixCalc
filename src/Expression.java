f8955220633c3fad3140e1ff9ddadaaefeaabe0b
Max <MaxMB15@gmail.com>
2017-09-26 23:50:24 -0400

    another quick update to get plans going in future

--- src/Expression.java
+++ src/Expression.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 
 //This class is in charge of handling multiple NumberVar and Operands
 public class Expression {
 	public Expression(String s){
 		//I have to implement a parentheses stack
-		//One teset
+		ArrayList<Expression> stack = new ArrayList<Expression>();
+		//I have to get a way to get the closest brackets to get into the stack first... Will finish when I have more time
+		//mcgill assignments keeping me busy
 		
 		
 		
